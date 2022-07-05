f2eb0fa8c3bdda163132335ef625cf23120ac6c6
LucasReiche <lucas.reiche@mercedes-benz.com>
2022-07-05 15:22:29 +0200

    Initiate webpack and npm

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.js',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})],
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader','css-loader','sass-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader,'css-loader','sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
+    ]
+}
