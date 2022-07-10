3c4b8ffba21f767aba1b45a65b2bbfa04aa8ca2b
LucasReiche <lucas.reiche@mercedes-benz.com>
2022-07-10 15:46:06 +0200

    Add Travel Duration + remove unneeded code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,31 +3,41 @@ export async function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    let formDate = document.getElementById('traveldate').value
+    let formDate = document.getElementById('travelDate').value
+    let reFormDate = document.getElementById('returnDate').value
     let data = {
         text: formText,
-        date: formDate
+        date: formDate,
+        return: reFormDate
     }
     console.log(data)
 
     let date1 = Date.now()
     let date2 = new Date(data.date)
+    let date3 = new Date(data.return)
 
     console.log(date1)
     console.log(date2)
+    console.log(date3)
 
+    let timeSpan = date2 - date1
+    let timeSpanDays = timeSpan / (1000 * 3600 * 24)
+    timeSpanDays = Math.round((timeSpanDays + 0.5))
 
-    let Timespan = date2 - date1
-    let Timespan_days = Timespan / (1000 * 3600 * 24)
-    Timespan_days = Math.round(Timespan_days)
+    let timeDuration = date3 - date2
+    timeDuration = timeDuration / (1000 * 3600 * 24)
+    timeDuration = timeDuration + 1
 
     console.log("::: Travel Date is :::")
     console.log(date2)
     console.log("::: Timespan is :::")
-    console.log(Timespan_days)
+    console.log(timeSpanDays)
+    console.log("::: Timeduration is :::")
+    console.log(timeDuration)
 
     Object.assign(data, {
-        Timespan_days
+        timeSpanDays,
+        timeDuration
     })
 
     if (Client.checkForName(data) == true) {
@@ -46,15 +56,7 @@ export async function handleSubmit(event) {
     console.log("::: Before picture :::")
     console.log(data)
     await apiRequest2('http://localhost:8083/pictureApi', data)
-
-    // console.log("::: Form Submitted :::")
-    // fetch('http://localhost:8083/test')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     document.getElementById('results').innerHTML = res.message
-    // })
 }
-
 
 // Make async geonames POST request
 const apiRequest = async (url = '', data) => {
@@ -73,11 +75,11 @@ const apiRequest = async (url = '', data) => {
         const newData = await res.json();
         console.log("::: newData is here! :::");
         console.log(newData);
-        const message = JSON.stringify(newData)
         Object.assign(data, newData)
         console.log(data)
         document.getElementById('results_geo').innerHTML = data.name+"<br>"+data.country
-        document.getElementById('results_time').innerHTML = data.Timespan_days
+        document.getElementById('results_time').innerHTML = data.timeSpanDays
+        document.getElementById('results_duration').innerHTML = data.timeDuration
     } catch (error) {
         console.log("error", error);
     }
@@ -100,7 +102,6 @@ const apiRequest1 = async (url = '', data) => {
         const newData = await res.json();
         console.log("::: newData is here! :::");
         console.log(newData);
-        const message = JSON.stringify(newData)
         Object.assign(data, newData)
         document.getElementById('results_weather').innerHTML = data.midTemp+"<br>"+data.sky
     } catch (error) {
@@ -125,7 +126,6 @@ const apiRequest2 = async (url = '', data) => {
         const newData = await res.json();
         console.log("::: newData is here! :::");
         console.log(newData);
-        const message = JSON.stringify(newData)
         Object.assign(data, newData)
         document.getElementById('results_picture').innerHTML = `<img src=${data.picLink} alt=Picture of ${data.name}>`;
     } catch (error) {
